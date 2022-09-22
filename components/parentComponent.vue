44bd7804863cd9b4db54443baa0e96c0b12ee736
Simas Raudonis <srd@novicell.dk>
2022-09-22 15:43:01 +0200

    DYNAMIC SECTIONS WITH UNEVEN COL NO FIXED

--- components/parentComponent.vue
+++ components/parentComponent.vue
@@ -20,49 +20,63 @@
 
           <!-- TODO DEFINE ALL POSSBILE COL SELECTION OPTIONS for section
             67 - 33% HOW?!?!?!? mb on mounted trigger function and see what happens
-            33 - 57%
-
-
+            33 - 67%
           -->
-          <v-col cols="12"
-            :md="[(section.layout_settings.column_widths == '33-33-33' ? 4 : '') |
-            (section.layout_settings.column_widths == '50-50' ? 6 : '') |
-            // (section.layout_settings.column_widths == '67-33' ? this.doShit()  : '') |
-            (section.layout_settings.column_widths == '' ? 12 : '')]"
+
+          <v-col
+            cols="12"
+            :md="[
+              (section.layout_settings.column_widths == '33-33-33' ? 4 : '')
+                | (section.layout_settings.column_widths == '50-50' ? 6 : '')
+                | (section.layout_settings.column_widths == '67-33' ? assignColumns8to4(8) : '')
+                | (section.layout_settings.column_widths == '33-67' ? assignColumns4to4(4) : '') |
+                (section.layout_settings.column_widths == '' ? 12 : ''),
+            ]"
             sm="12"
-            v-for="region in section.regions"  :key="region.id">
+            v-for="region in section.regions"
+            :key="region.id"
+          >
             <!-- Dynamic classes constructor by @Soren on sections-->
             <v-row>
-            <v-col v-for="content in region" :key="content.id">
-              <!-- <h1> {{ content.bundle }} - type of component (content/block types)</h1> -->
+              <v-col v-for="content in region" :key="content.id">
+                <!-- <h1> {{ content.bundle }} - type of component (content/block types)</h1> -->
 
-              <!-- if content.build is article  -->
-              <Articles v-if="content.bundle === 'articles'" :data="content" />
+                <!-- if content.build is article  -->
+                <Articles
+                  v-if="content.bundle === 'articles'"
+                  :data="content"
+                />
 
-              <!-- rte component -->
-              <Rte v-else-if="content.bundle === 'rte'" :data="content" />
+                <!-- rte component -->
+                <Rte v-else-if="content.bundle === 'rte'" :data="content" />
 
-              <!-- quote component -->
-              <Quote v-else-if="content.bundle === 'quote'" :data="content" />
+                <!-- quote component -->
+                <Quote v-else-if="content.bundle === 'quote'" :data="content" />
 
-              <!-- button component -->
-              <Button v-else-if="content.bundle === 'button'" :data="content" />
+                <!-- button component -->
+                <Button
+                  v-else-if="content.bundle === 'button'"
+                  :data="content"
+                />
 
-              <!-- appetizer component -->
-              <Appetizer v-else-if="content.bundle === 'appetizer'" :data="content" />
+                <!-- appetizer component -->
+                <Appetizer
+                  v-else-if="content.bundle === 'appetizer'"
+                  :data="content"
+                />
 
-              <!-- image component -->
-              <imageComponent
-                v-else-if="content.bundle === 'image'"
-                :data="content"
-              />
+                <!-- image component -->
+                <imageComponent
+                  v-else-if="content.bundle === 'image'"
+                  :data="content"
+                />
 
-              <!-- hero component -->
-              <Hero v-else-if="content.bundle === 'hero'" :data="content" />
+                <!-- hero component -->
+                <Hero v-else-if="content.bundle === 'hero'" :data="content" />
 
-              <!-- only if none other match -->
-              <UnknownBlock v-else :data="content"></UnknownBlock>
-            </v-col>
+                <!-- only if none other match -->
+                <UnknownBlock v-else :data="content"></UnknownBlock>
+              </v-col>
             </v-row>
           </v-col>
         </v-row>
@@ -73,37 +87,65 @@
 
 <script>
 import axios from "axios";
-import Appetizer from "./appetizer.vue";
 
 export default {
-    data() {
-        return {
-            drupalData: [],
-            layoutBuilder: [],
-            eightToFour: 8,
-        };
+  data() {
+    return {
+      drupalData: [],
+      layoutBuilder: [],
+      unevenSection: null,
+    };
+  },
+  methods: {
+    async getDrupalData() {
+      axios
+        .get("https://headless.drupal.dk/?format=json&region=content")
+        .then((response) => {
+          this.drupalData = response.data;
+          this.layoutBuilder = this.drupalData[0].layout_builder__layout;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-        async getDrupalData() {
-            axios
-                .get("https://headless.drupal.dk/?format=json&region=content")
-                .then((response) => {
-                this.drupalData = response.data;
-                this.layoutBuilder = this.drupalData[0].layout_builder__layout;
-            })
-                .catch((error) => {
-                console.log(error);
-            });
-        },
-        doShit() {
-            // initial value is 8, if hit again its 4
-            return this.eightToFour = this.eightToFour == 8 ? 4 : 8;
-        },
+  },
+
+  mounted() {
+    this.getDrupalData();
+  },
+
+  computed: {
+    assignColumns8to4() {
+      // rotate between 4 and 8
+      let previouValue = null;
+      let newValue = null;
+      return function (value) {
+        if (previouValue === value) {
+          newValue = value === 4 ? 8 : 4;
+        } else {
+          newValue = value;
+        }
+        previouValue = newValue;
+        return newValue;
+      };
     },
-    mounted() {
-        this.getDrupalData();
+
+    assignColumns4to4() {
+      // rotate between 8 and 4
+      let previouValue = null;
+      let newValue = null;
+      return function (value) {
+        if (previouValue === value) {
+          newValue = value === 8 ? 4 : 8;
+        } else {
+          newValue = value;
+        }
+        previouValue = newValue;
+        return newValue;
+      };
     },
-    components: { Appetizer }
+
+  },
 };
 </script>
 
